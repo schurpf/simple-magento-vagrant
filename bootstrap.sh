d15f32f2c9b40683cf93a828d3e577f5b9e76891
schurpf <michael@schurpf.com>
2015-04-17 20:07:34 -0500

    change name of magerun

--- bootstrap.sh
+++ bootstrap.sh
@@ -115,4 +115,4 @@ fi
 cd /vagrant/httpdocs
 wget https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar
 chmod +x ./n98-magerun.phar
-sudo mv ./n98-magerun.phar /usr/local/bin/
+sudo mv ./n98-magerun.phar /usr/local/bin/magerun
